78ce1497d4d12744e07933cbb0d46a08c717ace5
Richard Velasquez <revelasquez666@gmail.com>
2021-09-23 16:28:51 -0400

    plan de pagos

--- app/Http/Controllers/Api/V1/LoanController.php
+++ app/Http/Controllers/Api/V1/LoanController.php
@@ -2137,7 +2137,8 @@ class LoanController extends Controller
                             $date_fin = Carbon::parse($loan->disbursement_date)->startOfMonth()->addMonth()->endOfMonth()->endOfDay();
                             $days = Carbon::parse($loan->disbursement_date)->diffInDays($date_fin);
                             $interest = LoanPayment::interest_by_days($days, $loan->interest->annual_interest, $balance);
-                            $capital = $payment - $interest;
+                            //$capital = $payment - $interest;
+                            $capital = $payment;
                         }
                     }
                     else{
