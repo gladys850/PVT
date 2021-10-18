bfd05c29c73a287ac3f7445824194ed04788859d
Noemi Ancari <noemiancari.na@gmail.com>
2021-10-18 14:52:12 -0400

    adicion de softdeletes periods

--- app/LoanPaymentPeriod.php
+++ app/LoanPaymentPeriod.php
@@ -3,9 +3,11 @@
 namespace App;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class LoanPaymentPeriod extends Model
 {
+    use SoftDeletes;
     public $timestamps = true;
     public $guarded = ['id'];
     public $fillable = ['year',
