82054253843ba066cf65f18f2e2c2bbc0af1e96e
Maritza <marycqmia@gmail.com>
2021-12-16 08:02:12 -0400

    cambio de variables en el registro de grantias

--- app/Http/Controllers/Api/V1/LoanGuaranteeRegisterController.php
+++ app/Http/Controllers/Api/V1/LoanGuaranteeRegisterController.php
@@ -11,21 +11,20 @@ use Illuminate\Support\Facades\Auth;
 class LoanGuaranteeRegisterController extends Controller
 {
     /**
-    * Registro de Garantía Actualizar o Crear el array de Objeto Registro de Garantía de préstamos
-    * @bodyParam affiliate_id integer ID required de afiliado. Example: 9              
+    * Registro Crear o Actualizar el array de Objeto Registro de Garantía de préstamos
+    * @bodyParam affiliate_id integer ID required de afiliado. Example: 9  
+    * @bodyParam role_id integer required rol con el que se realiza el registro Example: 89            
     * @bodyParam guarantees[0].id  integer required ID del registro de la tabla loans o prestamos.Example: 1
-    * @bodyParam guarantees[0].type string required registro del nombre de las tablas tabla loans,prestamos. Example: prestamos
-    * @bodyParam guarantees[0].type_affiliate enum required (lender,guarantor,cosigner) Example:guarantor
+    * @bodyParam guarantees[0].type string required registro del nombre de las tablas tabla loans,prestamos. Example: PVT
+    * @bodyParam guarantees[0].code string required codigo del prestamo perteneciente a la garantia Example: P-00001
     * @bodyParam guarantees[0].quota numeric required cuota del prestamo de los garantes Example: 10000.50
-    * @bodyParam guarantees[0].type_adjust enum required (loan_guarantee_register)  Example: loan_guarantee_register
     * @bodyParam guarantees[1].id  integer required ID del registro de la tabla loans,prestamos.Example: 1
-    * @bodyParam guarantees[1].type string required registro del nombre de las tablas tabla loans,prestamos. Example: prestamos
-    * @bodyParam guarantees[1].type_affiliate enum required (lender,guarantor,cosigner) Example:guarantor
+    * @bodyParam guarantees[1].type string required registro del nombre de las tablas tabla loans,prestamos. Example: SISMU
+    * @bodyParam guarantees[1].code string required codigo del prestamo perteneciente a la garantia Example: P-00002
     * @bodyParam guarantees[1].quota numeric required cuota del prestamo de los garantes Example: 10000.50
-    * @bodyParam guarantees[1].type_adjust enum required (loan_guarantee_register)  Example: loan_guarantee_register
-    
+
     * @authenticated
-    * @responseFile responses/loan_contribution_adjust/updateOrCreateLoanGuaranteeRegister.200.json
+    * @responseFile responses/loan_guarantee_register/updateOrCreateLoanGuaranteeRegister.200.json
     */
     public function updateOrCreateLoanGuaranteeRegister(Request $request) { 
           $request->validate([
@@ -33,36 +32,35 @@ class LoanGuaranteeRegisterController extends Controller
             'role_id' => 'required|integer|exists:roles,id',
             'guarantees' => 'required|array'
           ]);
-         $loan_contribution_guarantee_register_ids = collect();
-          foreach ($request->guarantees as $loan_contribution_adjust) {
-              $adjustable_type = $loan_contribution_adjust['type'];
-              $loan_contribution_guarantee_register = new LoanGuaranteeRegister();
-              $loan_contribution_guarantee_register->user_id = Auth::id();
-              $loan_contribution_guarantee_register->role_id = $request->role_id;
-              $loan_contribution_guarantee_register->guarantable_id = $loan_contribution_adjust['id'];
-              $loan_contribution_guarantee_register->guarantable_type = $loan_contribution_adjust['type']=="SISMU" ? "prestamos":"loans";
-              $loan_contribution_guarantee_register->affiliate_id = $request->affiliate_id;
-              $loan_contribution_guarantee_register->amount = $loan_contribution_adjust['quota'];
-              $loan_contribution_guarantee_register->loan_code_guarantee = $loan_contribution_adjust['code'];
-              $loan_contribution_guarantee_register->period_date = Carbon::now()->format('Y-m-d');
-              $loan_contribution_guarantee_register->database_name = $adjustable_type;
-              $adjust_contribution = LoanGuaranteeRegister::where('affiliate_id',$request->affiliate_id)
-                                                    ->where('guarantable_type',$loan_contribution_guarantee_register->guarantable_type)
-                                                    ->where('guarantable_id',$loan_contribution_guarantee_register->guarantable_id)
-                                                    ->where('loan_code_guarantee',$loan_contribution_guarantee_register->loan_code_guarantee)
-                                                    ->where('period_date',$loan_contribution_guarantee_register->period_date)
-                                                    ->where('database_name',$loan_contribution_guarantee_register->database_name)
-                                                    ->whereNull('loan_id')->first();  
-            //return $adjust_contribution;                           
-              if($adjust_contribution){
-                  $adjust_contribution->update();
-                  $loan_contribution_guarantee_register_ids->push($adjust_contribution->id);
+         $loan_guarantee_register_ids = collect();
+          foreach ($request->guarantees as $loan_guarantee) {
+              $guarantee_type = $loan_guarantee['type'];
+              $loan_guarantee_register = new LoanGuaranteeRegister();
+              $loan_guarantee_register->user_id = Auth::id();
+              $loan_guarantee_register->role_id = $request->role_id;
+              $loan_guarantee_register->guarantable_id = $loan_guarantee['id'];
+              $loan_guarantee_register->guarantable_type = $loan_guarantee['type']=="SISMU" ? "prestamos":"loans";
+              $loan_guarantee_register->affiliate_id = $request->affiliate_id;
+              $loan_guarantee_register->amount = $loan_guarantee['quota'];
+              $loan_guarantee_register->loan_code_guarantee = $loan_guarantee['code'];
+              $loan_guarantee_register->period_date = Carbon::now()->format('Y-m-d');
+              $loan_guarantee_register->database_name = $guarantee_type;
+              $loan_guarantee = LoanGuaranteeRegister::where('affiliate_id',$request->affiliate_id)
+                                                    ->where('guarantable_type',$loan_guarantee_register->guarantable_type)
+                                                    ->where('guarantable_id',$loan_guarantee_register->guarantable_id)
+                                                    ->where('loan_code_guarantee',$loan_guarantee_register->loan_code_guarantee)
+                                                    ->where('period_date',$loan_guarantee_register->period_date)
+                                                    ->where('database_name',$loan_guarantee_register->database_name)
+                                                    ->whereNull('loan_id')->first();                          
+              if($loan_guarantee){
+                  $loan_guarantee->update();
+                  $loan_guarantee_register_ids->push($loan_guarantee->id);
               }else{
-                 $loan_contribution_guarantee_register->save();
-                 $loan_contribution_guarantee_register_ids->push($loan_contribution_guarantee_register->id);
+                 $loan_guarantee_register->save();
+                 $loan_guarantee_register_ids->push($loan_guarantee_register->id);
               }                   
           }
-          $loan_contribution_guarantee_register_ids= array('loan_contribution_guarantee_register_ids'=>$loan_contribution_guarantee_register_ids);
-          return $loan_contribution_guarantee_register_ids;
+          $loan_guarantee_register_ids = array('loan_guarantee_register_ids'=>$loan_guarantee_register_ids);
+          return $loan_guarantee_register_ids;
       }
 }
