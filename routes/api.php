31aa93ac80a1c83b4de6b534f79a051d9a07252f
bdquispe <daysi.quispe444@gmail.com>
2022-03-21 13:10:34 -0400

    Voucher Loan v1

--- routes/api.php
+++ routes/api.php
@@ -13,6 +13,7 @@ Route::group([
     Route::get('affiliate/{affiliate}/fingerprint', 'Api\V1\AffiliateController@fingerprint_saved');//TODO biometrico VERIFICAR RUTA ABIERTA 
     Route::get('affiliate/{affiliate}/deletefingerprint', 'Api\V1\AffiliateController@fingerprint_delete');//b
     Route::post('evaluate_garantor2', 'Api\V1\CalculatorController@evaluate_guarantor2');
+    Route::get('list_voucher', 'Api\V1\VoucherController@loan_voucher');
     // INDEFINIDO (TODO)
     Route::get('document/{affiliate_id}', 'Api\V1\ScannedDocumentController@create_document');
     Route::get('generate_plans', 'Api\V1\LoanController@generate_plans');
@@ -115,6 +116,7 @@ Route::group([
         Route::get('get_list_year', 'Api\V1\LoanPaymentPeriodController@get_list_year');//listado de meses por gestion
         Route::apiResource('periods', 'Api\V1\LoanPaymentPeriodController')->only('index', 'show', 'store', 'update', 'destroy');//cambiar a cobranzas
         Route::post('loan/update_loan_affiliates', 'Api\V1\LoanController@update_loan_affiliates');
+                             
         //Movimientos de fondo Rotatorio
         Route::group([
             'middleware' => 'permission:closing-movement-fund-rotatory'
@@ -321,6 +323,13 @@ Route::group([
             Route::apiResource('voucher', 'Api\V1\VoucherController')->only('destroy');
             Route::patch('voucher/{voucher_id}/delete','Api\V1\VoucherController@delete_voucher_payment');              
         });
+        // Voucher Tesoreria
+        Route::group([
+            'middleware' => 'show-list-voucher'
+        ], function () {
+            Route::get('loan_voucher', 'Api\V1\VoucherController@loan_voucher');
+            });
+
         // Dirección
         Route::group([
             'middleware' => 'permission:create-address'
