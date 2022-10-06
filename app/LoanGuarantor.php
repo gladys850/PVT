62325e1eaab6d1a4ebdf659d6c0f883648efbca7
revelasquez <70278522+revelasquez@users.noreply.github.com>
2022-10-06 17:38:32 -0400

    ajuste de boletas

--- app/LoanGuarantor.php
+++ app/LoanGuarantor.php
@@ -106,8 +106,8 @@ class LoanGuarantor extends Model
         {
           if(Contribution::find($is_ballot_id))
             $ballots->push(Contribution::find($is_ballot_id));
-          if(LoanContributionAdjust::where('adjustable_id', $is_ballot_id)->where('loan_id',$this->id)->first())
-            $adjusts->push(LoanContributionAdjust::where('adjustable_id', $is_ballot_id)->where('loan_id',$this->id)->first());
+            if(LoanContributionAdjust::where('adjustable_id', $is_ballot_id)->where('loan_id',$this->loan_id)->first())
+            $adjusts->push(LoanContributionAdjust::where('adjustable_id', $is_ballot_id)->where('loan_id',$this->loan_id)->first());
         }
         $count_records = count($ballots);               
         foreach($ballots as $ballot)
