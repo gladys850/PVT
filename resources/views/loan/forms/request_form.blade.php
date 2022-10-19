3b9f4f2c3b7bfcf6efb17aa4464be5bb250013f2
richard <revelasquez666@gmail.com>
2022-10-19 15:43:21 -0400

    formulario de calificacion

--- resources/views/loan/forms/request_form.blade.php
+++ resources/views/loan/forms/request_form.blade.php
@@ -188,20 +188,22 @@
                 @else 
                 @php ($pasivo_guarantor = true )
                 @endif
-                <td>Categoría</td>
-                @if ($inactive)
-                <td colspan="{{$pasivo_guarantor ? 2 : 1}}">Tipo de Renta</td>
+                @if($guarantor->type == 'affiliates')
+                    <td>Categoría</td>
+                    @if ($inactive)
+                    <td colspan="{{$pasivo_guarantor ? 2 : 1}}">Tipo de Renta</td>
+                    @endif
+                </tr>
+                <tr>
+                @if ($guarantor->affiliate_state->affiliate_state_type->name != "Pasivo")
+                    <td class="data-row py-5" colspan="{{$inactive ? 1 : 2}}">{{ $guarantor->full_unit}}</td>
                 @endif
-            </tr>
-            <tr>
-            @if ($guarantor->affiliate_state->affiliate_state_type->name != "Pasivo")
-                <td class="data-row py-5" colspan="{{$inactive ? 1 : 2}}">{{ $guarantor->full_unit}}</td>
-            @endif
-                <td class="data-row py-5">{{ $guarantor->category ? $guarantor->category->name : '' }}</td>
-                @if ($inactive)
-                    <td colspan="{{$pasivo_guarantor ? 2 : 1}}" class="data-row py-5">{{ $guarantor->pension_entity ? $guarantor->pension_entity->name :''}}</td>
+                    <td class="data-row py-5">{{ $guarantor->category ? $guarantor->category->name : '' }}</td>
+                    @if ($inactive)
+                        <td colspan="{{$pasivo_guarantor ? 2 : 1}}" class="data-row py-5">{{ $guarantor->pension_entity ? $guarantor->pension_entity->name :''}}</td>
+                    @endif
+                </tr>
                 @endif
-            </tr>
             @endif
         </table>
         @endforeach
