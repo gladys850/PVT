1e607998dabe3360c8d3af4d13c4e1d63da1653a
gladys850 <gpq850@gmail.com>
2023-01-09 16:36:20 -0400

    migracion add_column_loan_procedure_id

---
+++ database/migrations/2023_01_09_153008_add_column_loan_procedure_id.php
@@ -0,0 +1,53 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class AddColumnLoanProcedureId extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        // Schema::table('loan_global_parameters', function (Blueprint $table) {
+        //     $table->unsignedInteger('min_amount_fund_rotary')->change();
+        //     $table->bigInteger('loan_procedure_id')->nullable();
+        //     $table->foreign('loan_procedure_id')->references('id')->on('loan_procedures');
+        // });
+
+        // Schema::table('loans', function (Blueprint $table) {
+        //     $table->bigInteger('loan_procedure_id')->nullable();
+        //     $table->foreign('loan_procedure_id')->references('id')->on('loan_procedures');
+        // });
+
+        Schema::table('loan_modality_parameters', function (Blueprint $table) {
+            $table->bigInteger('loan_procedure_id')->nullable();
+            $table->foreign('loan_procedure_id')->references('id')->on('loan_procedures');
+            $table->unique(['procedure_modality_id', 'loan_procedure_id']);
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('loan_global_parameters', function (Blueprint $table) {
+            //
+        });
+
+        Schema::table('loans', function (Blueprint $table) {
+            //
+        });
+
+        Schema::table('loan_modality_parameters', function (Blueprint $table) {
+            //
+        });
+    }
+}
