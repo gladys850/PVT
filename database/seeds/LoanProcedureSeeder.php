a707d808532792c506a3e4e4a618b9d4cd832815
Richard Velasquez <revelasquez666@gmail.com>
2023-01-13 15:17:07 -0400

    formato fecha

--- database/seeds/LoanProcedureSeeder.php
+++ database/seeds/LoanProcedureSeeder.php
@@ -13,8 +13,8 @@ class LoanProcedureSeeder extends Seeder
     public function run()
     {
         $loan_porcedures = [
-            ['description'=>'Reglamento de Préstamos 2019','start_production_date'=>'04-06-2021','is_enable'=>false],
-            ['description'=>'Reglamento de Préstamos 2022','start_production_date'=>'16-01-2023','is_enable'=>true],
+            ['description'=>'Reglamento de Préstamos 2019','start_production_date'=>'2021-06-04','is_enable'=>false],
+            ['description'=>'Reglamento de Préstamos 2022','start_production_date'=>'2023-01-16','is_enable'=>true],
             ];
             foreach ($loan_porcedures as $loan_porcedure) {
                 LoanProcedure::firstOrCreate($loan_porcedure);
