1fda2a944c0e781a89c956b9c4651da41ea013c4
MaxLyonel <leorexx100@gmail.com>
2023-02-02 14:45:27 -0400

    implementación de colas en PVT

--- app/Http/Controllers/Api/V1/SMSController.php
+++ app/Http/Controllers/Api/V1/SMSController.php
@@ -6,6 +6,7 @@ use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 use App\Helpers\Util;
 use App\Loan;
+use App\Jobs\ProcessNotificationSMS;
 
 /** @group Notificaciones
 * Datos de los trámites de préstamos y sus relaciones
@@ -25,21 +26,15 @@ class SMSController extends Controller
         $loan_id = $request->loan_id;
         $user_id = $request->user_id;
         $message = "SE SOLICITA SE APERSONE POR NUESTRAS OFICINAS DE LA MUSERPOL A OBJETO DEL RECOJO Y/O FIRMA DEL CONTRATO DE SU PRÉSTAMO.";
+        $notification_type = 5; // tipo de notificación: Contrato de préstamo
 
         $cell_phone_number = Loan::find($loan_id)->borrower->first()->cell_phone_number;
         $cell_phone_number = Util::remove_special_char($cell_phone_number);
-        if(Util::delegate_shipping($cell_phone_number, $message, $loan_id, $user_id)) {
-            return response()->json([
-                'error' => false,
-                'message' => 'Se envío el SMS correctamente',
-                'data' => []
-            ]);
-        } else {
-            return response()->json([
-                'error' => true,
-                'message' => 'Hubo un error en el envío de SMS',
-                'data' => []
-            ]);
-        }
+        ProcessNotificationSMS::dispatch($cell_phone_number, $message, $loan_id, $user_id, $notification_type);
+        return response()->json([
+            'error' => false,
+            'message' => 'Se esta notificando al afiliado!',
+            'data' => []
+        ]);
     }
 }
