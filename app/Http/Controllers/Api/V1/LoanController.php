f1d8da92981476787a52022608fd82886a8befe3
Noemi Ancari <noemiancari.na@gmail.com>
2023-04-11 11:17:03 -0400

    cambio de nombre a la nueva MAE con acentos

--- app/Http/Controllers/Api/V1/LoanController.php
+++ app/Http/Controllers/Api/V1/LoanController.php
@@ -1068,7 +1068,7 @@ class LoanController extends Controller
         $guarantors = $loan->borrowerguarantors;
         $lenders = $loan->borrower;
         $employees = [
-            ['position' => 'Director General Ejecutivo','name'=>'CNL. MSC. CAD. LUCIO ENRIQUE RENE JIMENEZ VARGAS','identity_card'=>'3475563'],
+            ['position' => 'Director General Ejecutivo','name'=>'CNL. MSC. CAD. LUCIO ENRIQUE RENÉ JIMÉNEZ VARGAS','identity_card'=>'3475563'],
             ['position' => 'Director de Asuntos Administrativos','name'=>'LIC. FRANZ LAZO CHAVEZ','identity_card'=>'3367169 LP']
         ];
         $data = [
