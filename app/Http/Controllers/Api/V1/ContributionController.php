145cfc296f9f86505df2a66ac9c2cb1210ca2e93
bdquispe <daysi.quispe444@gmail.com>
2024-08-12 18:05:59 -0400

    Impresión del Comprobante de Boleta de Pago

--- app/Http/Controllers/Api/V1/ContributionController.php
+++ app/Http/Controllers/Api/V1/ContributionController.php
@@ -89,8 +89,8 @@ class ContributionController extends Controller
                     ['Hora', Carbon::now()->format('h:m:s a')]
                 ]
             ],
-            'title' => 'COMPROBANTE DE PAGO',
-            'subtitle' => '(SEGÚN IMPORTACIÓN DE PLANILLA DE HABERES - COMANDO GENERAL DE LA POLICÍA BOLIVIANA)',
+            'title' => 'COMPROBANTE DE BOLETA DE PAGO',
+            'subtitle' => 'SEGÚN IMPORTACIÓN PLANILLA DE HABERES - COMANDO GENERAL DE LA POLICÍA BOLIVIANA',
             'period' => $period,
             'affiliate' => $affiliate,
             'contribution' => $contribution
